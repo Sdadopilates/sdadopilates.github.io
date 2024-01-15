c135d51627ed90613f3aec0ee8338993df679c31
John Carlson <jawnnypoo@gmail.com>
2024-01-15 02:48:08 -0600

    Fonts fonts fonts

--- lib/styles.dart
+++ lib/styles.dart
@@ -1,26 +1,35 @@
 import 'package:flutter/material.dart';
 
 class Styles {
+
+  static const fontPTSerif = "PTSerif";
+
   static const headerTitle = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontFamily: fontPTSerif,
     color: Colors.white,
     fontSize: 50.0,
   );
 
   static const headerSubtitle = TextStyle(
     color: Colors.white,
+    fontFamily: fontPTSerif,
     fontSize: 30.0,
   );
 
   static const title = TextStyle(
     fontWeight: FontWeight.bold,
+    fontFamily: fontPTSerif,
     fontSize: 20.0,
   );
 
   static const body = TextStyle(
+    fontFamily: fontPTSerif,
     fontSize: 16.0,
   );
 
   static const buttonStyle = TextStyle(
+    fontFamily: fontPTSerif,
     fontSize: 30.0,
   );
 }
